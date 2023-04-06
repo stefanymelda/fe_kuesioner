55c2c725f37a1f0c419ebf50fd99c9b31228699b
stefanymelda <juwitastefany13@gmail.com>
2023-04-06 15:16:12 +0700

    fe_kuesioner

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,22 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { setInner } from "https://bukulapak.github.io/element/process.js";
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "./table.js";
 let urlAPI = "https://jaehyun.herokuapp.com/kuesioner";
 get(urlAPI, isiTableKuesioner);
 function isiTableKuesioner(results) {
-  console.log(results);
   results.forEach(isiRow);
 }
 function isiRow(value) {
-  console.log(value);
+  let content = isiTabel
+    .replace("#NAMA#", value.biodata.nama)
+    .replace("#NOHP#", value.phone_number)
+    .replace("#JENISKELAMIN#", value.biodata.jenis_kelamin)
+    .replace("#EMAIL#", value.biodata.email)
+    .replace("#USIA#", value.usia)
+    .replace("#STATUS#", value.status)
+    .replace("#LOKASI#", value.biodata.location)
+    .replace("#WARNA#", getRandomColor())
+    .replace(/#WARNALOGO#/g, getRandomColorName());
+  addInner("iniTabel", content);
 }
