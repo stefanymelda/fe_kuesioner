7c75d5d8acd7ffddc20739618def698d4b98820c
stefanymelda <juwitastefany13@gmail.com>
2023-06-09 11:59:59 +0700

    cobaTB

---
+++ js/controller/post.js
@@ -0,0 +1,25 @@
+import { postData } from "https://bukulapak.github.io/api/process.js";
+import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
+import { urlPOST, AmbilResponse } from "../config/url_post.js";
+
+function pushData() {
+  var biodata = getValue("biodata");
+
+  let data = {
+    longitude: parseFloat(getValue("longitude")),
+    latitude: parseFloat(getValue("latitude")),
+    location: getValue("location"),
+    email: getValue("email"),
+    status: getValue("status"),
+    biodata: {
+      nama: getValue("nama"),
+      jenis_kelamin: getValue("jenis_kelamin"),
+      usia: getValue("usia"),
+      email: getValue("email"),
+      phone_number: getValue("phone_number"),
+    },
+  };
+  postData(urlPOST, data, AmbilResponse);
+}
+
+onClick("button", pushData);
