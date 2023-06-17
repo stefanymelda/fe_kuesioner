d5c23753a77e2609847a9a768e342a61c040e84e
stefanymelda <juwitastefany13@gmail.com>
2023-06-17 13:40:15 +0700

    dd

--- js/controller/edit.js
+++ js/controller/edit.js
@@ -3,7 +3,7 @@ export function isiData(results) {
       { id: 'nama', path: 'biodata.nama' },
       { id: 'jenis_kelamin', path: 'biodata.jenis_kelamin' },
       { id: 'phone_number', path: 'biodata.phone_number' },
-      { id: 'email', path: 'biodata.email' },
+      { id: 'email', path: 'email' },
       { id: 'usia', path: 'biodata.usia' },
       { id: 'location', path: 'location' },
       { id: 'latitude', path: 'latitude' },
@@ -11,20 +11,20 @@ export function isiData(results) {
       { id: 'status', path: 'status' },
     ];
   
-    inputMapping.forEach(({ id, path, index, property }) => {
+    inputMapping.forEach(({ id, path }) => {
       const inputElement = document.getElementById(id);
-      const value = getNestedValue(results, path, index, property);
+      const value = getNestedValue(results, path);
       inputElement.value = value;
     });
   }
   
-  function getNestedValue(obj, path, index, property) {
+  function getNestedValue(obj, path) {
     const value = path.split('.').reduce((value, key) => (value && value[key]) ? value[key] : '', obj);
     // console.log(`Value at path ${path}:`, value);
   
-    if (Array.isArray(value) && value.length > index && value[index].hasOwnProperty(property)) {
-      return value[index][property];
-    }
+    // if (Array.isArray(value) && value.length > index && value[index].hasOwnProperty(property)) {
+    //   return value[index][property];
+    // }
   
     return value;
   }
