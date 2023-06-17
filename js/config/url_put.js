9e8a5f687d66200eeaae5aa2e62e1d2c9c24ef31
stefanymelda <juwitastefany13@gmail.com>
2023-06-17 13:12:43 +0700

    bisa

---
+++ js/config/url_put.js
@@ -0,0 +1,10 @@
+const urlParams = new URLSearchParams(window.location.search);
+const kuesionerId = urlParams.get("kuesionerId");
+
+export let urlPUT = "https://jaehyun.herokuapp.com/update/" + kuesionerId;
+
+export function AmbilResponse(result) {
+  console.log(result); // menampilkan response API pada console
+  alert(result.message); // menampilkan response API pada alert
+  window.location.href = "index.html"; // reload halaman setelah klik ok pada alert
+}
