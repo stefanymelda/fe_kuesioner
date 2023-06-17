0f97fea53f2e121f5c5088206074b09c2c42ccd4
stefanymelda <juwitastefany13@gmail.com>
2023-06-17 16:26:39 +0700

    bisaah

--- js/temp/table.js
+++ js/temp/table.js
@@ -20,7 +20,7 @@ export let isiTabel = `
     <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#STATUS#</th>
     <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#LOKASI#</th>
     <th class="whitespace-nowrap pr-4 bg-white text-sm font-medium text-coolGray-800">
-<a type="button" href="edit.html?presensiId=#IDEDIT#"> Edit
+<a type="button" href="edit.html?kuesionerId=#IDEDIT#"> Edit
 </a>
 |
 <button type="button" id="del_button" onclick="deleteData('#IDHAPUS#')"> Delete
